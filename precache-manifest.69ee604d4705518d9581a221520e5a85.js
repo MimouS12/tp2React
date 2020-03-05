dfbc2e86528664143431065a89ddc984a9fa2d42
Maryem Souayah <maryemsouayah3@gmail.com>
2020-03-05 23:04:11 +0100

    Updates

---
+++ precache-manifest.69ee604d4705518d9581a221520e5a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011af2094635e14888006ed8a6c6fc0b",
+    "url": "/tp2React/index.html"
+  },
+  {
+    "revision": "b32ec23caf1dd29dfec7",
+    "url": "/tp2React/static/css/main.c169d246.chunk.css"
+  },
+  {
+    "revision": "e07556476a4bcf7c062e",
+    "url": "/tp2React/static/js/2.b934c97b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tp2React/static/js/2.b934c97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32ec23caf1dd29dfec7",
+    "url": "/tp2React/static/js/main.e2f4b40b.chunk.js"
+  },
+  {
+    "revision": "8d2152ac1b697b9e1f53",
+    "url": "/tp2React/static/js/runtime-main.0afae384.js"
+  }
+]);
